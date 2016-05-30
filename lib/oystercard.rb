8155be2b0883069a09c2120ecd5253272f0243f8
Van Le <singularity@vannio.co.uk>
2016-05-30 11:42:23 +0100

    Added tests for topping up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,23 @@
 class Oystercard
+  MAXIMUM_BALANCE = 9000 # in pence
+  ERROR = {
+    exceed_limit: "Balance is over the maximum limit of £#{MAXIMUM_BALANCE / 100}"
+  }
+
   attr_reader :balance
 
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    fail (ERROR[:exceed_limit]) if limit_exceeded?(amount)
+    @balance += amount
+  end
+
+  private
+
+    def limit_exceeded?(amount)
+      @balance + amount > MAXIMUM_BALANCE
+    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,18 @@ describe Oystercard do
   it "has an initial balance of 0" do
     expect(subject.balance).to eq(0)
   end
+
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it "is topped up by a specified amount" do
+      expect { subject.top_up(1000) }.to change { subject.balance }.by(1000)
+    end
+
+    it "does not allow topping up beyond the maximum balance limit" do
+      expect do
+        subject.top_up(described_class::MAXIMUM_BALANCE + 1)
+      end.to raise_error(described_class::ERROR[:exceed_limit])
+    end
+  end
 end
