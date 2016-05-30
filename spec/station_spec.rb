a04e30d6f7a900ef9a0643541b7a1b413e9586e5
Van Le <singularity@vannio.co.uk>
2016-05-30 16:19:17 +0100

    Stations have a name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,4 +1,15 @@
 require "station"
 
 describe Station do
+  subject { described_class.new("Manor House", 2) }
+
+  describe "#initialize" do
+    it "has a name on initialisation" do
+      expect(subject.name).to eq("Manor House")
+    end
+
+    it "has a zone on initialisation" do
+      expect(subject.zone).to be_integer
+    end
+  end
 end
