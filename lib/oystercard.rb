ef16c16b97e7bc94c11116365c96edd4f549f0ef
LukaszGr <lukasz.grychowski@gmail.com>
2016-05-30 22:02:32 +0100

    ch7 deduct added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -12,4 +12,8 @@ class Oystercard
     fail "Maximum balance of #{MAX_BALANCE} reached!" if balance + amount > MAX_BALANCE
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,6 +9,10 @@ describe Oystercard do
     expect(subject).to respond_to(:top_up).with(1).argument
   end
 
+  it "it responds to deduct with argument" do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
   describe "#top_up" do
     it "adds to the balance" do
       expect{subject.top_up(1)}.to change{subject.balance}.by(1)
@@ -17,6 +21,12 @@ describe Oystercard do
       max_balance = Oystercard::MAX_BALANCE
       subject.top_up(max_balance)
       expect{subject.top_up(1)}.to raise_error "Maximum balance of #{max_balance} reached!"
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts from the balance" do
+      expect{subject.deduct(1)}.to change{subject.balance}.by(-1)
     end
   end
 end
