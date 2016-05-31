240beeb6e484e6a54109ad31ffbe589221b7c742
Van Le <singularity@vannio.co.uk>
2016-05-31 15:36:07 +0100

    Oystercard tops up and deducts fare

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,31 @@
 class Oystercard
-  attr_reader :balance
+  MAXIMUM_LIMIT = 90
+  MINIMUM_LIMIT = 1
+  MAX_LIM_ERR_MSG = "£#{MAXIMUM_LIMIT} is the maximum limit"
+  MIN_LIM_ERR_MSG = "£#{MINIMUM_LIMIT} is the minimum limit"
+
+  attr_reader :balance, :in_journey
+  alias in_journey? in_journey
 
   def initialize
     @balance = 0
+  end
+
+  def top_up(amount)
+    fail MAX_LIM_ERR_MSG if @balance + amount > MAXIMUM_LIMIT
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    fail MIN_LIM_ERR_MSG if @balance < MINIMUM_LIMIT
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,47 @@ describe Oystercard do
       expect(oystercard.balance).to_not be(nil)
     end
   end
+
+  describe "#top_up" do
+    it "takes a number" do
+      expect { oystercard.top_up(10) }.to change { oystercard.balance }.by(10)
+    end
+
+    it "won't accept a topup over the limit" do
+      max = Oystercard::MAXIMUM_LIMIT
+      expect { oystercard.top_up(max + 1) }.to raise_error
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts an amount" do
+      expect { oystercard.deduct(10) }.to change { oystercard.balance }.by(-10)
+    end
+  end
+
+  describe "#touch_in" do
+    before do
+      oystercard.top_up(Oystercard::MINIMUM_LIMIT)  
+    end
+
+    it "begins the journey" do
+      oystercard.touch_in
+      expect(oystercard).to be_in_journey
+    end
+
+    context "low balance" do
+      subject(:oystercard_low_balance) { described_class.new }
+
+      it "throws an error if balance is too low" do
+        expect { oystercard_low_balance.touch_in }.to raise_error
+      end
+    end
+  end
+
+  describe "#touch_out" do
+    it "ends the journey" do
+      oystercard.touch_out
+      expect(oystercard).to_not be_in_journey
+    end
+  end
 end
