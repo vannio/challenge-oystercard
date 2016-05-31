390d1e63ae01eafed678196fd8c860b795904889
LukaszGr <lukasz.grychowski@gmail.com>
2016-05-31 17:22:55 +0100

    ch13 created Station class

---
+++ lib/station.rb
@@ -0,0 +1,10 @@
+class Station
+
+  attr_reader :name, :zone
+
+  def initialize(station, zone)
+    @name = station
+    @zone = zone
+  end
+
+end
---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station  do
+
+  subject(:station){described_class.new("Bank", 1)}
+
+  context "when new station created" do
+    it "has a name" do
+      expect(subject.name).to eq "Bank"
+    end
+    it "has a zone" do
+      expect(subject.zone).to eq 1
+    end
+  end
+end
