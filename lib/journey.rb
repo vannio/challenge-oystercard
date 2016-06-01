2b5f92fb45c6538a4ed81dc06abcb048c0ec8323
Matt Butler <mattb-w@hotmail.co.uk>
2016-06-01 14:19:21 +0100

    finished journey station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,26 @@
+require 'oystercard'
+
 class Journey
-  attr_reader :entry_station
+
+  PENALTY_FARE = 6
+
+  attr_reader :entry_station, :exit_station
+
   def initialize(entry_station = nil)
     @entry_station = entry_station
+    @exit_station = nil
+  end
+
+  def finish(station = nil)
+    @exit_station = station
+    self
+  end
+
+  def complete?
+    entry_station && exit_station
+  end
+
+  def fare
+    complete? ? Oystercard::MIN_FARE : PENALTY_FARE
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,14 +1,64 @@
 require 'journey'
+require 'oystercard'
 
 
 describe Journey do
   let (:station) {double :station}
-  subject(:journey) {described_class.new}
+  let(:other_station) { double :other_station }
 
   context "given an entry station" do
+
     subject(:journey) {described_class.new(station)}
+
     it "has an entry station" do
       expect(journey.entry_station).to eq station
     end
+
+    it 'journey is complete' do
+      expect(journey).not_to be_complete
+    end
+
+    it 'fare should return minimum fare' do
+      expect(journey.fare).to eq (Journey::PENALTY_FARE)
+    end
+
+    it 'returns itself when finish' do
+      expect(journey.finish(other_station)).to eq journey
+    end
+  end
+
+  context "given an exit station" do
+
+    subject(:journey) {described_class.new}
+
+    before{journey.finish(other_station)}
+
+    it 'has an exit station' do
+        expect(journey.exit_station).to eq other_station
+    end
+
+    it 'journey is complete' do
+      expect(journey).not_to be_complete
+    end
+
+    it 'fare should return minimum fare' do
+      expect(journey.fare).to eq (Journey::PENALTY_FARE)
+    end
+  end
+
+  context "given both an entry and exit station" do
+
+    subject(:journey) {described_class.new(station)}
+
+    before {journey.finish(other_station)}
+
+    it 'journey is complete' do
+      expect(journey).to be_complete
+    end
+
+    it 'fare should return minimum fare' do
+      expect(journey.fare).to eq (Oystercard::MIN_FARE)
+    end
+
   end
 end
