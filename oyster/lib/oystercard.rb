91dd54ce9f218f58f8ada449b001b8e624d36a78
Randy Marmer <rmarmer1@yahoo.com>
2016-06-01 11:54:49 +0100

    begining day three

--- oyster/lib/oystercard.rb
+++ oyster/lib/oystercard.rb
@@ -1,10 +1,11 @@
 
 class Oystercard
 
-	attr_reader :balance
+	attr_reader :balance, :in_journey
 
 	def initialize
 		@balance = 0
+		@in_journey = false
 	end
 
 	def top_up(amount)
@@ -18,7 +19,10 @@ class Oystercard
 
 	def deduct(amount)
 		@balance -= amount
-		
+	end
+
+	def in_journey?
+		@in_journey
 	end
 
 end
--- oyster/spec/oystercard_spec.rb
+++ oyster/spec/oystercard_spec.rb
@@ -34,7 +34,24 @@ describe Oystercard do
 				end
 			end
 
-		end
+			describe '#in_journey?' do  
 
+				it 'raises an error if not currently traveling' do
+					expect { subject.top_up(91) }.to raise_error "Can't add to your balance; would breach the £90 limit"
+				end
+				
+				it 'necessarilly has a starting value of false; not in-transit' do 
+					expect(subject.in_journey?).to eq false
+				end
+
+				# it 'has a value of true after being used to \'touch in\', but not yet to \'touch out' do
+				# 	expect(subject.in_journey).to eq true
+				# end
+			end
+
+			describe '#touch_in' do
+
+			end
+		end
 	end
 end
