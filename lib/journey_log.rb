3c6385e351b72d45a0c2b3f6a98136399c90c347
Thomas Harker <thomasharker@Thomass-MacBook-Pro.local>
2016-06-02 09:50:01 +0100

    Journey log restarted

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,12 +1,4 @@
 require_relative 'journey'
 
 class JourneyLog
-  def initialize
-    @journeys = []
-  end
-
-  def start(station)
-    @current_journey = @journey_class.new(station)
-  end
-
 end
